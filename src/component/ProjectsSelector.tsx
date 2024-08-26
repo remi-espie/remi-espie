ca193254bca188ff8e09da696194867d7e0f5571
ESPIE <remi.espie@etu.umontpellier.fr>
2024-08-26 11:44:24 +0200

    feat: new selector for ssr

--- src/component/ProjectsSelector.tsx
+++ src/component/ProjectsSelector.tsx
@@ -7,11 +7,14 @@ import {
     Typography,
     useTheme,
 } from '@suid/material'
-import { For } from 'solid-js'
+import { createSignal, For } from 'solid-js'
 import { Technologies } from '../i18n/technologies.ts'
 import MyLink from './MyLink.tsx'
 import MyCardMedia from './MyCardMedia.tsx'
 import { Motion } from 'solid-motionone'
+import { Select } from '@thisbeyond/solid-select'
+import '@thisbeyond/solid-select/style.css'
+import SelectStyle from '../css/selector.module.css'
 
 const theme = useTheme()
 
@@ -25,72 +28,89 @@ function GetCard(props: {
     }
 }) {
     return (
-        <Card
-            elevation={12}
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                transition: 'background-color 0.33s',
-                height: '100%',
-                justifyContent: 'space-between',
-            }}
-        >
-            <Box sx={{ display: 'flex' }}>
-                <Box
+        <Grid item xs={4} sx={{ p: 2 }}>
+            <Motion
+                initial={{ opacity: 1, scale: 1 }}
+                animate={{ opacity: 0, scale: 0.6 }}
+                transition={{ duration: 0.5 }}
+                inView={{ opacity: 1, scale: 1 }}
+                inViewOptions={{
+                    once: true,
+                }}
+                style={{
+                    height: '100%',
+                }}
+            >
+                <Card
+                    elevation={12}
                     sx={{
                         display: 'flex',
                         flexDirection: 'column',
+                        transition: 'background-color 0.33s',
+                        height: '100%',
+                        justifyContent: 'space-between',
                     }}
                 >
-                    <CardContent
-                        sx={{
-                            flex: '1 0 auto',
-                        }}
-                    >
-                        <Typography component="div" variant="h5">
-                            {props.item.title}
-                        </Typography>
-                        <Typography
-                            variant="subtitle1"
-                            color="text.secondary"
-                            component="div"
+                    <Box sx={{ display: 'flex' }}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}
                         >
-                            {props.item.description}
-                        </Typography>
-                    </CardContent>
-                </Box>
-                <MyCardMedia img={props.item.image} />
-            </Box>
-            <Box sx={{ m: 1 }}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        flexWrap: 'wrap',
-                        gap: 1,
-                    }}
-                >
-                    <For each={props.item.technologies}>
-                        {(tech) => (
-                            <Chip
-                                label={tech}
+                            <CardContent
                                 sx={{
-                                    backgroundColor: Technologies.find((t) => {
-                                        return t.name === tech
-                                    })?.color,
+                                    flex: '1 0 auto',
                                 }}
-                            />
-                        )}
-                    </For>
-                </Box>
-                <MyLink
-                    to={props.item.url}
-                    text={props.item.url}
-                    target={'_blank'}
-                    color={theme.palette.primary.main}
-                />
-            </Box>
-        </Card>
+                            >
+                                <Typography component="div" variant="h5">
+                                    {props.item.title}
+                                </Typography>
+                                <Typography
+                                    variant="subtitle1"
+                                    color="text.secondary"
+                                    component="div"
+                                >
+                                    {props.item.description}
+                                </Typography>
+                            </CardContent>
+                        </Box>
+                        <MyCardMedia img={props.item.image} />
+                    </Box>
+                    <Box sx={{ m: 1 }}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                gap: 1,
+                            }}
+                        >
+                            <For each={props.item.technologies}>
+                                {(tech) => (
+                                    <Chip
+                                        label={tech}
+                                        sx={{
+                                            backgroundColor: Technologies.find(
+                                                (t) => {
+                                                    return t.name === tech
+                                                }
+                                            )?.color,
+                                        }}
+                                    />
+                                )}
+                            </For>
+                        </Box>
+                        <MyLink
+                            to={props.item.url}
+                            text={props.item.url}
+                            target={'_blank'}
+                            color={theme.palette.primary.main}
+                        />
+                    </Box>
+                </Card>
+            </Motion>
+        </Grid>
     )
 }
 
@@ -104,85 +124,56 @@ function ProjectsSelector(props: {
         url: string
     }[]
 }) {
-    // const [techList, setTechList] = createSignal<string[]>([])
-
-    // const handleChange = (event: SelectChangeEvent<string[]>) => {
-    //     const {
-    //         target: { value },
-    //     } = event
-    //     setTechList(value)
-    // }
-
-    // False positive
+    // not needed
     // eslint-disable-next-line solid/reactivity
-    // const [projects, setProjects] = createSignal(props.projectsList)
+    const [techList, setTechList] = createSignal<string[]>([...props.techs])
 
-    // createEffect(() => {
-    //     setProjects(
-    //         props.projectsList.filter((project) =>
-    //             techList().every((tech) => project.technologies.includes(tech))
-    //         )
-    //     )
-    // })
+    // not needed
+    // eslint-disable-next-line solid/reactivity
+    const [projects, setProjects] = createSignal(props.projectsList)
+
+    const [selectedTechs, setSelectedTechs] = createSignal<string[]>([])
+
+    function handleSelectChange(value: string[]) {
+        setSelectedTechs(value)
+
+        setTechList(() => {
+            return [...props.techs].filter(
+                (tech) => !selectedTechs().includes(tech)
+            )
+        })
+
+        setProjects(() => {
+            return props.projectsList.filter((project) =>
+                selectedTechs().every((tech) =>
+                    project.technologies.includes(tech)
+                )
+            )
+        })
+    }
 
     return (
         <>
-            {/*<FormControl*/}
-            {/*    sx={{*/}
-            {/*        m: 'auto',*/}
-            {/*        width: '80vw',*/}
-            {/*        maxWidth: 400,*/}
-            {/*        display: 'flex',*/}
-            {/*    }}*/}
-            {/*>*/}
-            {/*    <InputLabel>Tech</InputLabel>*/}
-            {/*    <Select*/}
-            {/*        multiple*/}
-            {/*        value={techList()}*/}
-            {/*        onChange={handleChange}*/}
-            {/*        input={<OutlinedInput label="Tech" />}*/}
-            {/*        MenuProps={{*/}
-            {/*            disableScrollLock: true,*/}
-            {/*            PaperProps: {*/}
-            {/*                style: {*/}
-            {/*                    'max-height': '400px',*/}
-            {/*                },*/}
-            {/*            },*/}
-            {/*        }}*/}
-            {/*        sx={{*/}
-            {/*            backgroundColor: theme.palette.background.default,*/}
-            {/*        }}*/}
-            {/*    >*/}
-            {/*        <For each={[...props.techs]}>*/}
-            {/*            {(tech) => <MenuItem value={tech}>{tech}</MenuItem>}*/}
-            {/*        </For>*/}
-            {/*    </Select>*/}
-            {/*</FormControl>*/}
+            <Box
+                style={{
+                    '--bg-color': theme.palette.common.white,
+                    '--text-color': theme.palette.common.black,
+                }}
+                class={SelectStyle.select}
+            >
+                <Select
+                    multiple
+                    options={[...techList()]}
+                    onChange={handleSelectChange}
+                    placeholder={'Technologies...'}
+                />
+            </Box>
             <Grid
                 container
                 sx={{ width: '80vw', m: 'auto', mb: 32 }}
                 columns={{ xs: 2, sm: 8, md: 12 }}
             >
-                <For each={props.projectsList}>
-                    {(item) => (
-                        <Grid item xs={4} sx={{ p: 2 }}>
-                            <Motion
-                                initial={{ opacity: 1, scale: 1 }}
-                                animate={{ opacity: 0, scale: 0.6 }}
-                                transition={{ duration: 0.3 }}
-                                inView={{ opacity: 1, scale: 1 }}
-                                inViewOptions={{
-                                    once: true,
-                                }}
-                                style={{
-                                    height: '100%',
-                                }}
-                            >
-                                <GetCard item={item} />
-                            </Motion>
-                        </Grid>
-                    )}
-                </For>
+                <For each={projects()}>{(item) => <GetCard item={item} />}</For>
             </Grid>
         </>
     )
