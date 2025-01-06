7d2bb308d7bf1e2ff643f67f6187dceba4d35572
ESPIE <remi.espie@etu.umontpellier.fr>
2025-01-06 12:49:58 +0100

    fix: vinxi 0.4.3

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@solidjs/start/config'
+import suidPlugin from '@suid/vite-plugin'
+
+export default defineConfig({
+    vite: {
+        plugins: [suidPlugin()],
+        build: {
+            target: 'esnext',
+        },
+        assetsInclude: ['**/*.jpg', '**/*.png', '**/*.md'],
+    },
+})
