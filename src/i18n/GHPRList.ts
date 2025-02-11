f66da08cf3c12c5eda0e7b46cf2ed22150051473
ESPIE <remi.espie@etu.umontpellier.fr>
2025-02-11 17:13:27 +0100

    feat: add GitHub contribution

--- src/i18n/GHPRList.ts
+++ src/i18n/GHPRList.ts
@@ -4,4 +4,5 @@ export const GHPRList = [
     'https://github.com/hashicorp/packer-plugin-hyperv/pull/115',
     'https://github.com/hashicorp/packer-plugin-virtualbox/pull/124',
     'https://github.com/hashicorp/packer-plugin-vmware/pull/199',
+    'https://github.com/K-Phoen/backstage-plugin-confluence/pull/174',
 ]
